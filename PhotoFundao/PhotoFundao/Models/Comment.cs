330f0e61efb1f4432b5ea0c7d7dd18db51fa10fc
Fabricio <Fabricio@mind>
2015-09-02 22:15:48 -0300

    Comment

--- PhotoFundao/PhotoFundao/Models/Comment.cs
+++ PhotoFundao/PhotoFundao/Models/Comment.cs
@@ -7,6 +7,12 @@ namespace PhotoFundao.Models
 {
     public class Comment
     {
+        public int CommentID { get; set; }
+        public int PhotoID { get; set; }
+        public string UserName { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public virtual Photo Photo { get; set; }
 
     }
 }
